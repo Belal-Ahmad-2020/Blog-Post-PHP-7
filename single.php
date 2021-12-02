0b5c2a21ac0e45d03dc41c650c867bcfc3c49218
Belal-Ahmad-2020 <belal.mansoori7@gmail.com>
2021-12-02 22:41:28 +0430

    Single Page

---
+++ single.php
@@ -0,0 +1,231 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Single Post</title>
+
+
+<!-- font awesome -->
+<link rel="stylesheet" href="fonts/css/fontawesome.css">
+<link rel="stylesheet" href="fonts/css/all.css">
+
+<!-- fonts    candal  lora google fonts -->
+<!-- <link  href="fonts/Candal-Regular.ttf">
+<link  href="fonts/Lora-Italic-VariableFont_wght.ttf"> -->
+
+
+<!-- style -->
+<link rel="stylesheet" href="css/style.css">
+
+
+
+</head>
+<body>
+
+ <!--navigation bar  -->
+<header>
+    <div class="logo">
+        <h1 class="logo-text">
+            <span>KU</span> Developers
+        </h1>
+    </div>
+<!-- end navaigation bar -->
+
+    <!-- menu icon -->
+        <i class="fa fa-bars  menu-toggle"></i>
+    <!-- end menu icon -->
+
+    <!-- menu navbar -->
+    <ul class="nav">
+        <li>
+            <a href="#">Home</a>
+            <!-- <ul class="drop-down" style="">
+                <li><a href="#">Landing Page</a></li>
+                <li><a href="#" >Team</a></li>
+            </ul> -->
+        </li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Services</a></li>
+        <!-- <li><a href="#">Sign Up</a></li>
+        <li><a href="#">Login</a></li> -->
+        <li><a href="#">
+            <i class="fa fa-user"></i>
+            Awa Melvine
+            <i class="fa fa-chevron-down fa-sm"></i>
+        </a>
+            <ul class="drop-down">
+                <li><a href="#">Dashboard</a></li>
+                <li><a href="#" class="logout">Logout</a></li>
+            </ul>
+        </li>
+    </ul>
+    <!-- menu navbar -->
+
+</header>
+
+<!-- closure or slide show  page wrapper -->
+<div class="page-wrapper">
+
+     <!-- content -->
+        <div class="content clearfix">
+<!-- main content  wrapper-->
+        <div class="main-content-wrapper">
+            <div class="main-content single">
+                <h1 class="post-title">This is the title of the post.</h1>
+                <div class="post-content">
+                    <p> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsam, excepturi debitis! Voluptas explicabo ea sequi.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque est natus laborum dolores pariatur. Neque ducimus explicabo delectus, hic velit possimus, ipsa quidem ipsam asperiores et rem animi. Magnam, voluptates?</p>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vitae, doloremque beatae dolorem illum vel reiciendis repudiandae blanditiis perspiciatis minima impedit, odio non minus aliquid in, maxime ducimus. Cum nisi perspiciatis quia, assumenda beatae aperiam eos consequatur rem nesciunt totam a?</p>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta, magni?</p>
+                    <p> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsam, excepturi debitis! Voluptas explicabo ea sequi.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque est natus laborum dolores pariatur. Neque ducimus explicabo delectus, hic velit possimus, ipsa quidem ipsam asperiores et rem animi. Magnam, voluptates?</p>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vitae, doloremque beatae dolorem illum vel reiciendis repudiandae blanditiis perspiciatis minima impedit, odio non minus aliquid in, maxime ducimus. Cum nisi perspiciatis quia, assumenda beatae aperiam eos consequatur rem nesciunt totam a?</p>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta, magni?</p>
+                    <p> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsam, excepturi debitis! Voluptas explicabo ea sequi.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque est natus laborum dolores pariatur. Neque ducimus explicabo delectus, hic velit possimus, ipsa quidem ipsam asperiores et rem animi. Magnam, voluptates?</p>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vitae, doloremque beatae dolorem illum vel reiciendis repudiandae blanditiis perspiciatis minima impedit, odio non minus aliquid in, maxime ducimus. Cum nisi perspiciatis quia, assumenda beatae aperiam eos consequatur rem nesciunt totam a?</p>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta, magni?</p>
+                    <p> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsam, excepturi debitis! Voluptas explicabo ea sequi.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque est natus laborum dolores pariatur. Neque ducimus explicabo delectus, hic velit possimus, ipsa quidem ipsam asperiores et rem animi. Magnam, voluptates?</p>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vitae, doloremque beatae dolorem illum vel reiciendis repudiandae blanditiis perspiciatis minima impedit, odio non minus aliquid in, maxime ducimus. Cum nisi perspiciatis quia, assumenda beatae aperiam eos consequatur rem nesciunt totam a?</p>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta, magni?</p>
+                    <p> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsam, excepturi debitis! Voluptas explicabo ea sequi.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque est natus laborum dolores pariatur. Neque ducimus explicabo delectus, hic velit possimus, ipsa quidem ipsam asperiores et rem animi. Magnam, voluptates?</p>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vitae, doloremque beatae dolorem illum vel reiciendis repudiandae blanditiis perspiciatis minima impedit, odio non minus aliquid in, maxime ducimus. Cum nisi perspiciatis quia, assumenda beatae aperiam eos consequatur rem nesciunt totam a?</p>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta, magni?</p>
+
+                </div>
+            </div>
+        </div>    
+<!--  end of the main content -->
+
+<!-- sid4bar -->
+            <div class="sidebar single">
+
+                <div class="section popular">
+                    <h2 class="section-tilte">Popular</h2>
+
+                    <div class="post clearfix">
+                        <img src="images/img_1.jpg" alt="">
+                        <a href="#" class="title"> <h4>How to overcome your fears </h4></a>
+                    </div>
+
+                    <div class="post clearfix">
+                        <img src="images/img_1.jpg" alt="">
+                        <a href="#" class="title"> <h4>How to overcome your fears </h4></a>
+                    </div>
+
+                    <div class="post clearfix">
+                        <img src="images/img_1.jpg" alt="">
+                        <a href="#" class="title"> <h4>How to overcome your fears </h4></a>
+                    </div>
+
+                    <div class="post clearfix">
+                        <img src="images/img_1.jpg" alt="">
+                        <a href="#" class="title" > <h4>How to overcome your fears </h4></a>
+                    </div>
+
+                    <div class="post clearfix">
+                        <img src="images/img_1.jpg" alt="">
+                        <a href="#" class="title"> <h4>How to overcome your fears </h4></a>
+                    </div>
+
+                    <div class="post clearfix">
+                        <img src="images/img_1.jpg" alt="">
+                        <a href="#" class="title"> <h4>How to overcome your fears </h4></a>
+                    </div>
+
+                </div>
+
+
+                <div class="section topics">
+                    <h2 class="section-title">Topics</h2>
+                    <ul>
+                        <li><a href="#">Poems</a></li>
+                        <li><a href="#">Quotes</a></li>
+                        <li><a href="#">Fixtion</a></li>
+                        <li><a href="#">Biography</a></li>
+                        <li><a href="#">Motivation</a></li>
+                        <li><a href="#">Inspiration</a></li>
+                        <li><a href="#">Life Lessons</a></li>
+                    </ul>
+                </div>
+
+            </div>
+<!-- end of the side bar -->
+
+        </div>
+     <!-- end of the content -->
+</div>
+<!-- end of the closure page wrapper -->
+
+
+
+<!-- footer -->
+<div class="footer">    
+
+    <div class="footer-content">
+
+        <div class="footer-section about">
+            <h1 class="logo-text"><span>KU</span> Developers</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis cupiditate ullam fugiat inventore! Saepe ipsam deleniti animi fugiat quam laboriosam dolore. Commodi est facilis ad, ullam iure odio vero! Suscipit. </p>
+            <div class="contact">
+                <span> <i class="fas fa-phone"></i> &nbsp; +93764135179 </span>
+                <span><i class="fas fa-envelope"></i>&nbsp; info@kudevelopers.com</span>
+            </div>
+            <div class="socials">
+                <a href="#"><i class="fab fa-facebook"></i></a>
+                <a href="#"><i class="fab fa-instagram"></i></a>
+                <a href="#"><i class="fab fa-twitter"></i></a>
+                <a href="#"><i class="fab fa-youtube"></i></a>
+            </div>
+        </div>
+
+        <div class="footer-section links">
+            <h2>Quick Links</h2>
+            <ul>
+                <li><a href="#">Events </a></li>
+                <li><a href="#">Team </a></li>
+                <li><a href="#">Mentores </a></li>
+                <li><a href="#">Gallery      </a></li>
+                <li><a href="#">TErms and Conditions </a></li>
+            </ul>
+        </div>
+        <div class="footer-section contact-form">
+            <h2>Countact us</h2>
+            <form action="index.html" method="post" autocomplete="off">
+                <input type="email" name="email" id="email" 
+                class="text-input contact-input" placeholder="Your Email Address...">
+                <textarea rows='5' name="message" class="text-input contact-input" 
+                placeholder="Your Message..."></textarea>
+                <button type="submit" class="btn btn-big contact-btn">
+                <i class="fas fa-envelope"></i>    Send
+                </button>
+            </form>
+        </div>
+    </div>    
+
+    <div class="footer-bottom">
+        &copy kuDevelopers.com | Designed By Belal Ahmad Mansoori
+    </div>
+
+</div>
+<!-- end of the footer -->
+
+
+
+
+<!-- jquery -->
+<script src="js/jquery/jquery.min.js"></script>
+
+<!-- slick coursel -->
+<script src="https://"></script> 
+<!-- video 4 11:00 -->
+
+<!-- js codes -->
+<script src="js/script.js"></script>
+
+
+</body>
+</html>
